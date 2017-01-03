000059e0a662ede8d22c3393b75f143f95dddc25
michael dudley <michael.dudley@improvingenterprises.com>
2017-01-02 20:57:05 -0600

    global assembly info

--- Source/Improving.DbUp.Tests/Properties/AssemblyInfo.cs
+++ Source/Improving.DbUp.Tests/Properties/AssemblyInfo.cs
@@ -1,15 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Improving.DbUp.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Improving.DbUp.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("47826E09-A4D6-400E-872C-DC8B7D6F0AFD")]
 
